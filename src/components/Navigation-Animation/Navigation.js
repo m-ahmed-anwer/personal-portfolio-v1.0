37e3f896125e57ae83eab17a5b4d6342658f19db
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-02-02 17:22:53 +0530

    Update mobile ui

--- src/components/Navigation-Animation/Navigation.js
+++ src/components/Navigation-Animation/Navigation.js
@@ -62,6 +62,7 @@ function Navigation(props) {
         checked={isChecked}
         onChange={handleToggle}
       />
+
       <label htmlFor="page-nav-toggle">
         <svg className="icon--menu-toggle" viewBox="0 0 60 30">
           <g className="icon-group">
